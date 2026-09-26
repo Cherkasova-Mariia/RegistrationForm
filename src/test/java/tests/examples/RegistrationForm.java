6c169309c2e9353986c8a3b1af4a0a6df19c2989
Mariia Cherkasova <mariacerkasova2707@gmail.com>
2026-09-26 17:37:25 +0300

    add6

--- src/test/java/tests/examples/RegistrationForm.java
+++ src/test/java/tests/examples/RegistrationForm.java
@@ -23,10 +23,10 @@ public class RegistrationForm extends TestBase {
 
         $("[id=userNumber]").setValue("1234567890");
 
-        //$("[id=dateOfBirthInput]").click();
-      //  $("[id=react-datepicker__month-select]").$(byText("September")).click();
-       // $("[id=react-datepicker__month-select]").$(byText("1983")).click();
-      //  $("[id=react-datepicker__day--015]").click();
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1983");
+        $(".react-datepicker__day--015").click();
 
         $("[id=subjectsInput]").setValue("English").pressEnter();
         $("[id=hobbies-checkbox-1]").click();
@@ -41,12 +41,17 @@ public class RegistrationForm extends TestBase {
         $("[id=submit]").click();
 
 
-        $("[id=output] [id=modal-title]").shouldHave(text("Thanks for submitting the form"));
-        $("[id=output] [id=email]").shouldHave(text("mary023@gmail.com"));
-        $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
-        $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
-        $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));    }
-
-
+        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Mary Cher"));
+        $(".table-responsive").shouldHave(text("mary023@gmail.com"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("1234567890"));
+        $(".table-responsive").shouldHave(text("15 September,1983"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("test.txt"));
+        $(".table-responsive").shouldHave(text("First address"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
 
     }
+}
