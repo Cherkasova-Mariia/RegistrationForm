18b591410153310e7a9d1c7d54572e24e5e22077
Mariia Cherkasova <mariacerkasova2707@gmail.com>
2026-09-26 16:08:27 +0300

    add5

--- src/test/java/tests/examples/RegistrationForm.java
+++ src/test/java/tests/examples/RegistrationForm.java
@@ -3,6 +3,8 @@ package tests.examples;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -17,29 +19,34 @@ public class RegistrationForm extends TestBase {
         $("[id=firstName]").setValue("Mary");
         $("[id=lastName]").setValue("Cher");
         $("[id=userEmail]").setValue("mary023@gmail.com");
-        $("[id='gender-radio-2']").click();
+        $("[id=gender-radio-2]").click();
 
         $("[id=userNumber]").setValue("1234567890");
 
         //$("[id=dateOfBirthInput]").click();
-      //  $("[id=react-datepicker__month-select]").$(byText("September")).click();;
-       // $("[id=react-datepicker__month-select]").$(byText("1983")).click();;
-      //  $("[id==eact-datepicker__day--015]").click();
+      //  $("[id=react-datepicker__month-select]").$(byText("September")).click();
+       // $("[id=react-datepicker__month-select]").$(byText("1983")).click();
+      //  $("[id=react-datepicker__day--015]").click();
 
         $("[id=subjectsInput]").setValue("English").pressEnter();
         $("[id=hobbies-checkbox-1]").click();
-        $("[id=uploadPicture]").uploadFromClasspath("test.txt");
+        $("[id=uploadPicture]").uploadFile(new File("src/test/resources/test.txt"));
         $("[id=currentAddress]").setValue("First address");
 
-        $("[id=react-select-3-live-region]").selectOption("NCR");
-        $("[id=react-select-4-live-region]").selectOption("Delhi");
+        $("[id=state]").click();
+        $("[id=state]").$(byText("NCR")).click();
+        $("[id=city]").click();
+        $("[id=city]").$(byText("Delhi")).click();
+
         $("[id=submit]").click();
 
 
-
-        $("[id=output] [id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $("[id=output] [id=modal-title]").shouldHave(text("Thanks for submitting the form"));
         $("[id=output] [id=email]").shouldHave(text("mary023@gmail.com"));
         $("[id=output] [id=currentAddress]").shouldHave(text("first address 1"));
         $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));
+        $("[id=output] [id=permanentAddress]").shouldHave(text("second address 2"));    }
+
+
+
     }
-}
